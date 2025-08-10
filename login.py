563bdf35d6a6b68adb9b2edef84f7dd3e6a51dea
bami02 <bamlakzed2025@gmail.com>
2025-08-10 22:31:19 +0300

    task 2 done

---
+++ login.py
@@ -0,0 +1,15 @@
+user_name=input("Enter a new user name: ")
+password=input("Enter a new password: ")
+
+print("    login   ")
+a=input("Enter user name: ")
+b=input("Enter password: ")
+
+if a==user_name and b==password:
+    print("login successful! Welcome!")
+elif a!=user_name:
+    print("User name doesn't match.Please try again.")
+elif b!=password:
+    print("Password doesn't match.Please try again.")
+
+print("thank you")
